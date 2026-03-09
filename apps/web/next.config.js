10586c8936889b862c31999c4460f0f6b9901271
Celo Composer <support@celo.org>
2026-03-08 21:42:03 -0600

    fix: Disable swcMinify to resolve Axios "t is not a function" error in Vercel builds.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false, // Fix Axios "t is not a function" error in Vercel built minified files
   transpilePackages: ['@human.tech/passport-embed'],
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
