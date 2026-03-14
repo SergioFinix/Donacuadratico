86fe08101a0010d27b8b4aafc782391a03b39f7b
Celo Composer <support@celo.org>
2026-03-14 02:40:58 -0600

    feat: redeploy QuadraticTipping contract with updated deployment artifacts.

--- apps/contracts/ignition/modules/QuadraticTipping.ts
+++ apps/contracts/ignition/modules/QuadraticTipping.ts
@@ -5,7 +5,7 @@ const USDC_ADDRESS_SEPOLIA = "0x01C5C0122039549AD1493B8220cABEdD739BC44E";
 const QuadraticTippingModule = buildModule("QuadraticTippingModule", (m) => {
     // Configurable parameters with defaults
     const usdcAddress = m.getParameter("usdcAddress", USDC_ADDRESS_SEPOLIA);
-    const verifierAddress = m.getParameter("verifierAddress", "0x0000000000000000000000000000000000000000"); // will be deployed by owner, who can be verifier initially
+    const verifierAddress = m.getParameter("verifierAddress", "0x09BB59c870AA5CB0e7A01b2f96d72B29f3a4BE90");
 
     const DonaCuadraticoping = m.contract("QuadraticTipping", [usdcAddress, verifierAddress]);
 
