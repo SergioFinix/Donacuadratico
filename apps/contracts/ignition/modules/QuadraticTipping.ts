2a6525d3d6c3dc8656cc6a1e36260e8ac4bdc11d
Celo Composer <support@celo.org>
2026-03-15 02:14:03 -0600

    feat: Update deployment scripts to target QuadraticTipping and record its new deployment address.

--- apps/contracts/ignition/modules/QuadraticTipping.ts
+++ apps/contracts/ignition/modules/QuadraticTipping.ts
@@ -5,7 +5,8 @@ const TOKEN_ADDRESS_MAINNET = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 const QuadraticTippingModule = buildModule("QuadraticTippingModule", (m) => {
     // Configurable parameters with defaults
     const usdcAddress = m.getParameter("usdcAddress", TOKEN_ADDRESS_MAINNET);
-    const verifierAddress = m.getParameter("verifierAddress", "0xf76A0Af73Df734393ca2684f7e7BB9b446aa0010");
+    const verifierAddress = m.getParameter("verifierAddress", "0x09BB59c870AA5CB0e7A01b2f96d72B29f3a4BE90");
+
 
     const DonaCuadraticoping = m.contract("QuadraticTipping", [usdcAddress, verifierAddress]);
 
